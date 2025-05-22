989c2a6c93adf24e8df4d9a5b4821127f5d41411
Mike Hyler <mhyler@zoho.com>
2025-05-22 14:31:06 -0400

    refactored from vanilla js to jQuery

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function newItem() {
+  let list = $("#list");
+
+  list.append("<li></li>");
+  let inputValue = $("#input").val();
+  let li = $("<li></li>").text(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+
+  $("#list").sortable();
+}
